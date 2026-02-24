53eaf6d30d31b5904f77d93b82c6e88ada6cc020
RH-CHON <183950125+RH-CHON@users.noreply.github.com>
2026-02-24 14:15:28 +0000

    Function for Missingness Heatmap

--- ukb_G1_missingness_check.R
+++ ukb_G1_missingness_check.R
@@ -25,3 +25,43 @@ print_high_missingness <- function(df, threshold = 0.30) {
 
 print_high_missingness(ukb)
 
+############################################
+# Produce missingness heatmap
+############################################
+missingness_heatmap <- function(df, bin_size = 100) {
+  
+  # Boolean transformation
+  na_matrix <- is.na(df) * 1
+  
+  # Define output dimensions and binning
+  n_bins <- ceiling(nrow(na_matrix) / bin_size)
+  row_indices <- rep(1:n_bins, each = bin_size, length.out = nrow(na_matrix))
+  
+  # Density calculation
+  agg_matrix <- rowsum(na_matrix, row_indices) / bin_size
+  
+  # Data reshaping
+  long_data <- melt(agg_matrix, varnames = c("Row_Bin", "Feature"))
+  
+  p <- ggplot(long_data, aes(x = Feature, y = Row_Bin, fill = value)) +
+    geom_raster() +
+    scale_fill_gradient(low = "grey90", high = "darkred", 
+                        name = "Missingness Proportion",
+                        limits = c(0, 1)) +
+    scale_y_continuous(expand = c(0,0)) +
+    scale_x_discrete(expand = c(0,0)) +
+    labs(title = "Missingness Density Heatmap",
+         x = "Variables",
+         y = sprintf("Aggregated Row Bins (1 bin = %d observations)", bin_size)) +
+    theme_minimal() +
+    theme(plot.title = element_text(hjust = 0.5),
+          axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1, size = 3), 
+          axis.ticks.x = element_line(color = "grey80"),
+          panel.grid = element_blank(),
+          legend.title = element_text(margin = margin(b = 15)))
+  
+  ggsave(filename = file.path("figure", "missingness_heatmap.png"), plot = p, 
+         width = 20, height = 12, dpi = 900)
+}
+
+missingness_heatmap(ukb, bin_size = 10000)
