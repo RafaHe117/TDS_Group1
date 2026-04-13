ea1896c77a94fa608332aed197bdbe1b4385c988
yl6025 <yl6025@ic.ac.uk>
2026-04-14 00:24:10 +0100

    Update 06_famd.sh

--- run_jobs/06_famd.sh
+++ run_jobs/06_famd.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+mkdir -p /rds/general/project/hda_25-26/live/TDS/anw16/TDS_Group1/modelling_script/06_FAMD/logs
 qsub /rds/general/project/hda_25-26/live/TDS/anw16/TDS_Group1/modelling_script/06_FAMD/run_famd_pipeline.sh
