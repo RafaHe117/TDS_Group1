2c9a03e93ec9dc854ee0b477c815766545f225b3
yl6025 <yl6025@ic.ac.uk>
2026-04-13 22:53:16 +0100

    Update table1_main_bysex_before_after_run.R

--- modelling_script/01_table1/table1_main_bysex_before_after_run.R
+++ modelling_script/01_table1/table1_main_bysex_before_after_run.R
@@ -26,7 +26,7 @@ source(file.path(table1_dir, "table1_utils.R"))
 # -----------------------------
 # Options
 # -----------------------------
-RENDER_PNG <- TRUE
+RENDER_PNG <- FALSE
 
 # Put all new outputs in a separate folder
 OUTDIR_BYSEX <- file.path(table1_dir, "output_main_bysex_before_after")
