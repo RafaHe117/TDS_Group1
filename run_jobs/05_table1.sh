ef58455ae3a65ea64415b0b374131081cf14483f
yl6025 <yl6025@ic.ac.uk>
2026-04-14 00:24:23 +0100

    Update 05_table1.sh

--- run_jobs/05_table1.sh
+++ run_jobs/05_table1.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+mkdir -p /rds/general/project/hda_25-26/live/TDS/anw16/TDS_Group1/modelling_script/01_table1/logs
 qsub /rds/general/project/hda_25-26/live/TDS/anw16/TDS_Group1/modelling_script/01_table1/run_table1_pipeline.sh
