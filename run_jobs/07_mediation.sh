14ebc836aa19fa3adfdc62a3c4d4238b7b5dc72f
yl6025 <yl6025@ic.ac.uk>
2026-04-14 16:11:42 +0100

    Update 07_mediation.sh

--- run_jobs/07_mediation.sh
+++ run_jobs/07_mediation.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mkdir -p /rds/general/project/hda_25-26/live/TDS/anw16/TDS_Group1/modelling_script/08_mediation/logs
-qsub /rds/general/project/hda_25-26/live/TDS/anw16/TDS_Group1/modelling_script/08_mediation/run_mediation_pipeline.sh
+mkdir -p /rds/general/project/hda_25-26/live/TDS/anw16/TDS_Group1/modelling_script/08_mediation_outdated/logs
+qsub /rds/general/project/hda_25-26/live/TDS/anw16/TDS_Group1/modelling_script/08_mediation_outdated/run_mediation_pipeline.sh
